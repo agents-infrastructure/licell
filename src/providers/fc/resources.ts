3714986429c5ad1a5a561a9e0068a7ff48db618e
dafang <didida@duck.com>
2026-06-24 08:18:01 +0800

    chore: clean fc resource constants formatting

--- src/providers/fc/resources.ts
+++ src/providers/fc/resources.ts
@@ -6,4 +6,3 @@ export const FC_DEFAULT_INSTANCE_CONCURRENCY = 10;
 export const FC_SUPPORTED_DISK_SIZE_MB = [512, 10240] as const;
 export const FC_MEMORY_VCPU_RATIO_MIN = 1;
 export const FC_MEMORY_VCPU_RATIO_MAX = 4;
-
